51c7532d58e25f5f0f07bdbb5a5cb0a8ae9180e5
Leonidas Loucas <lloucas@imvu.com>
2013-07-02 19:34:38 -0700

    fix os basedir imprt for py.py

--- raspberry_pi_scripts/py.py
+++ raspberry_pi_scripts/py.py
@@ -3,7 +3,7 @@ import sys
 import signal
 
 basedir=os.path.dirname(os.path.realpath(__file__))
-sys.path.append(basedir + './lib/Adafruit_Libraries/Adafruit_ADS1x15')
+sys.path.append(basedir + '/./lib/Adafruit_Libraries/Adafruit_ADS1x15')
 
 
 from Adafruit_ADS1x15 import ADS1x15
