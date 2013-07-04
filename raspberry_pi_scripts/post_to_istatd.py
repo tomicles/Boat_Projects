54e886d7148f1563ede5652d452f3a5f2a0f5aa5
Leonidas Loucas <lloucas@imvu.com>
2013-07-03 23:53:08 -0700

    remove post to istatd ip logic. fixed elsewhere with vpn

--- raspberry_pi_scripts/post_to_istatd.py
+++ raspberry_pi_scripts/post_to_istatd.py
@@ -11,8 +11,7 @@ from daemon import Daemon
 from functools import partial
 
 def get_ip():
-    res = sp.call("iwconfig | grep Mifi-Santana2023", shell=True)
-    return  "192.168.24.239" if res == 1 else socket.gethostbyaddr("hackercomplex.dyndns.org")[2][0]
+    return  "192.168.24.239" 
 
 def send_to_istatd(counter, data, sock, dest):
     sock.sendto("{counter} {data}\n".format(counter=counter, data=data), dest)
