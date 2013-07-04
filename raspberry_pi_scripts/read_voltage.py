13ba5aaaa4303cf53b544083d2e33e30a244c7bf
Leonidas Loucas <lloucas@imvu.com>
2013-07-03 21:29:23 -0700

    first try getting multiplied voltage and configuartion of a daemon

--- raspberry_pi_scripts/read_voltage.py
+++ raspberry_pi_scripts/read_voltage.py
@@ -6,6 +6,8 @@ import os
 
 from functools import partial
 import itertools
+from optparse import OptionParser
+import json
 
 from daemon import Daemon
 from util import dataWriter
@@ -17,29 +19,74 @@ from Adafruit_ADS1x15 import ADS1x15
 
 ADS1015 = 0x00 #12 bit ADC
 
+
+class VoltageCollector(object):
+    def __init__(self, argv):
+        parser = OptionParser()
+        parser.add_option(
+            "-c",
+            "--config",
+            dest="config",
+            default="/etc/voltage_daemon.cfg",
+            help="Config file"
+        )
+
+        (options, args) = parser.parse_args(argv) 
+        self.config = options.config
+        self.adc = ADS1x15(ic=ADS1015)
+
+        with open(self.config, 'r') as f:
+            self.configuration = json.loads(f.read())
+
+    def translate_volts(self, sensor):
+        """ for 
+        vin_true = 3.298
+        vout_true = .779
+        """
+
+        index, data = sensor
+        vout = float(data)
+        z1 = int(self.configuration[str(index)]['coefficient']['z1'])
+        z2 = int(self.configuration[str(index)]['coefficient']['z2'])
+        vin = vout * (z1+z2) / z2
+        return vin
+
+    def loop_once(self):
+        sensors = self.configuration.keys()
+
+        volts = map(lambda x: self.adc.readADCSingleEnded(x, 4096, 250) / 1000, sensors)
+        writer = partial(dataWriter, prefix='/var/tmp/data')
+        map(lambda (name, volt): writer(name, volt), zip(['voltage.raw.a{n}'.format(n=x) for x in sensors], volts))
+        map(lambda (name, volt): writer(name, volt), zip(['voltage.multiplied.a{n}'.format(n=x) for x in sensors], map(self.translate_volts, zip(sensors,volts))))
+
 class VoltageDaemon(Daemon):
     def run(self):
-        adc = ADS1x15(ic=ADS1015)
+        self.voltage_collector = VoltageCollector(sys.argv[1:])
         while True:
-            volts = map(lambda x: adc.readADCSingleEnded(x, 4096, 250) / 1000, [0,1,2,3])
-            writer = partial(dataWriter, prefix='/var/tmp/data')
-            map(lambda (name, volt): writer(name, volt), zip(['voltage.a{n}'.format(n=x) for x in [0,1,2,3]], volts))
+            self.voltage_collector.loop_once()
             time.sleep(1)
 
 
 def main(argv):
     myDaemon = VoltageDaemon('/tmp/voltage.pid')
+    daemon_args = argv
+    if '--' in argv:
+        index = argv.index('--')
+        app_args = [argv[0]] + argv[index+1:]
+        daemon_args = argv[:index]
+        sys.argv = app_args
 
-    if 'start' == argv[1]:
+
+    if 'start' == daemon_args[1]:
         myDaemon.start()
-    elif 'stop' == argv[1]:
+    elif 'stop' == daemon_args[1]:
         myDaemon.stop()
-    elif 'restart' == argv[1]:
+    elif 'restart' == daemon_args[1]:
         myDaemon.restart()
-    elif 'run' == argv[1]:
+    elif 'run' == daemon_args[1]:
         myDaemon.run()
     else:
-        print 'usage: {script} start|stop|restart|run'.format(script=argv[0])
+        print 'usage: {script} start|stop|restart|run'.format(script=daemon_args[0])
         sys.exit(2)
     sys.exit(0)
 
